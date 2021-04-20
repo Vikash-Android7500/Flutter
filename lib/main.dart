affcd016e3f6a0262c31dfd2d24df47eb3e17224
vikas <vikash75@gmail.com>
2021-04-20 13:45:49 +0530

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
-import 'package:fistday/home_page.dart';
+import 'package:fistday/HomeScreen.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+ void main() => runApp(MyApp());
+
 
 //class StatelessWidget
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return HomePage();
+    return MaterialApp(
+        home: HomeScreen()
+    );
   }
 }
