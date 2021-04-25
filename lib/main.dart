4564ebda736c02410e1006a5e659e677936b2e52
vikas <vikash75@gmail.com>
2021-04-25 16:24:28 +0530

    day5 finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,25 @@
-
 import 'package:fistday/screen/HomeScreen.dart';
 import 'package:fistday/screen/login_page.dart';
 import 'package:flutter/material.dart';
 
- void main(){
-   runApp(MyApp());
- }
-
+void main() {
+  runApp(MyApp());
+}
 
 //class StatelessWidget
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        // home: HomeScreen(),
-        themeMode: ThemeMode.light,
-        theme: ThemeData(primarySwatch: Colors.green),
-        darkTheme: ThemeData(brightness: Brightness.dark),
-        initialRoute: "/login",
-        routes: {
-          "/": (context) => HomeScreen(),
-          "/login": (context) => LoginPage(),
-        },
+      // home: HomeScreen(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.green),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/home",
+      routes: {
+        "/home": (context) => HomeScreen(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
--- lib/screen/HomeScreen.dart
+++ lib/screen/HomeScreen.dart
@@ -1,21 +1,36 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title:Text("Appbar")
-      ),
-      body: Center(    
-          child: Container( height: 100, width: 100, color: Colors.blueGrey,
-            child: Text("Home Screen", style: TextStyle(fontSize: 20,  color: Colors.white),),
+        appBar: AppBar(title: Text("Appbar")),
+        body: Center(
+          child: Text(
+            "Home Screen",
+            style: TextStyle(fontSize: 20, color: Colors.blue),
+          ),
         ),
-      ),
-      drawer: Drawer(
-        child: Text("Item-1", style: TextStyle(fontSize: 20,  color: Colors.lightBlueAccent),),
-      )
-      );
+        drawer: Drawer(
+          child: Column(
+            children: [
+              Center(
+                child: Image.asset("assets/images/myapp.png", width: 60, height: 60,),
+              ),
+              Column(
+                children: [
+                  Text("Java", style: TextStyle(color: Colors.redAccent, fontSize: 16,),),
+                  Text("Kotlin", style: TextStyle(color: Colors.green, fontSize: 16),),
+                  Text("Python", style: TextStyle(color: Colors.greenAccent, fontSize: 16),),
+                  Text("Dart", style: TextStyle(color: Colors.blue, fontSize: 16),),
+                  Text("Mysql", style: TextStyle(color: Colors.orangeAccent, fontSize: 16),),
+                  Text("Java Script", style: TextStyle(color: Colors.deepOrangeAccent, fontSize: 16),),
+                ],
+              )
+            ],
+          ),
+
+        ));
   }
 }
